6fc1672f814c7245284ed1877324b45a67c5bb80
TanishBhattaMain <tanishbhattaai@gmail.com>
2026-07-20 01:24:49 +0545

    Frontend: html and css completed

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('text-area');
+    const addTaskBtn = document.getElementById('task-btn');
+    const taskList = document.getElementById('task-list');
+
+
+})
