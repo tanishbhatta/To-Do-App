1bdf44c3fa37b0691c0f644bea3441bb73433e64
TanishBhattaMain <tanishbhattaai@gmail.com>
2026-07-22 23:53:49 +0545

    feat: javascript locked and css modification

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const taskInput = document.getElementById('text-area');
+    const taskInput = document.getElementById('add-task');
     const addTaskBtn = document.getElementById('task-btn');
     const taskList = document.getElementById('task-list');
+    const emptyImage = document.querySelector('.empty-image');
 
+    const toggleEmptyState = () => {
+        emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none' ;
+    }
+    const addTask = (event) => {
+        event.preventDefault();
+        const taskText = taskInput.value.trim();
+        if (!taskText){
+            return;
+        }
 
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox">
+        <span>${taskText}</span>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+        toggleEmptyState()
+    }
+    
+    addTaskBtn.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter'){
+            addTask(e);
+        }
+    })
 })
